a1ccad47847b744efd77a7a4e791a38275ba612f
lm175 <1756000830@qq.com>
2024-12-10 22:50:43 +0800

    更换为mysql数据库

--- src/plugins/ninja3/database.py
+++ src/plugins/ninja3/database.py
@@ -1,21 +1,30 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, BigInteger
 from .requires import Model
 
 from datetime import date
 
 
 class GameIDs(Model):
-    game_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int]
+    game_id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=False
+    )
+    user_id: Mapped[int] = mapped_column(BigInteger)
 
 
 class Users(Model):
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=False
+    )
     uid_nums: Mapped[int]
     need_remind: Mapped[bool]
 
 
 class GiftCodes(Model):
-    code: Mapped[str] = mapped_column(primary_key=True)
+    code: Mapped[str] = mapped_column(String(255), primary_key=True)
     time: Mapped[date]
     available: Mapped[bool]
