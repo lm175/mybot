4fea5c7a1c4f88e7b486b8deb563759b82008467
lm175 <1756000830@qq.com>
2025-03-05 02:26:01 +0800

    update

--- src/plugins/deepseek/const.py
+++ src/plugins/deepseek/const.py
@@ -7,14 +7,14 @@ if not dir_path.exists():
 character_path = dir_path / 'default_character.txt'
 if not character_path.exists():
     character_path.touch()
-with open(dir_path / 'default_character.txt', 'r', encoding='utf-8') as f:
+with open(character_path, 'r', encoding='utf-8') as f:
     character = f.read()
 
 blocklist_path = dir_path / 'blocklist.txt'
-if not character_path.exists():
-    character_path.touch()
-with open(blocklist_path, 'r', encoding='utf-8') as file:
-    blocklist = [line.strip() for line in file.readlines()]
+if not blocklist_path.exists():
+    blocklist_path.touch()
+with open(blocklist_path, 'r', encoding='utf-8') as f:
+    blocklist = [line.strip() for line in f.readlines()]
 
 
 morning_path = dir_path / 'morning'
