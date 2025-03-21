3294cb67131c929c0e47f915dbeb89c3e7a88dd1
lm175 <1756000830@qq.com>
2025-03-22 02:04:36 +0800

    delete function of gift codes

--- src/plugins/ninja3/dhm.py
+++ src/plugins/ninja3/dhm.py
@@ -22,134 +22,135 @@ dhm_update = on_command("更新兑换码", aliases={"更新礼包码"}, priority
 
 @dhm_update.handle()
 async def _(matcher: Matcher, code: Message = CommandArg()):
-    if code.extract_plain_text():
-        matcher.set_arg('code', code)
+    await dhm_update.send("由于官方兑换接口已下线，此功能将不再维护")
+#     if code.extract_plain_text():
+#         matcher.set_arg('code', code)
 
-@dhm_update.got('code', prompt='请输入兑换码')
-async def _(bot: Bot, code: str = ArgPlainText('code')):
-    code_exists = await db.query_one(
-        "SELECT code FROM giftcodes WHERE code=? LIMIT 1",
-        (code,)
-    )
-    if code_exists:
-        await dhm_update.finish('这个兑换码已经更新过啦~')
+# @dhm_update.got('code', prompt='请输入兑换码')
+# async def _(bot: Bot, code: str = ArgPlainText('code')):
+#     code_exists = await db.query_one(
+#         "SELECT code FROM giftcodes WHERE code=? LIMIT 1",
+#         (code,)
+#     )
+#     if code_exists:
+#         await dhm_update.finish('这个兑换码已经更新过啦~')
 
-    await dhm_update.send('检测中··')
-    available_servers, is_universal = await code_checker(code)
-    if available_servers:
-        await db.update_giftcode(code, available_servers)
-        servers_str = ''
-        for server_id in available_servers:
-            servers_str += f'{server_id + 1}, '
-        await dhm_update.send(f'更新成功，可用区服: {servers_str[:-2]}\n正在给已绑定的uid兑换，请耐心等待哦~')
-    else:
-        await dhm_update.finish('兑换码好像不太对呢，再检查一下吧')
+#     await dhm_update.send('检测中··')
+#     available_servers, is_universal = await code_checker(code)
+#     if available_servers:
+#         await db.update_giftcode(code, available_servers)
+#         servers_str = ''
+#         for server_id in available_servers:
+#             servers_str += f'{server_id + 1}, '
+#         await dhm_update.send(f'更新成功，可用区服: {servers_str[:-2]}\n正在给已绑定的uid兑换，请耐心等待哦~')
+#     else:
+#         await dhm_update.finish('兑换码好像不太对呢，再检查一下吧')
 
 
-    result: dict[int, str] = {}
-    success_count = 0
-    failure_count = 0
-    all_users = await db.query_all(
-        "SELECT user_id, need_remind FROM users",
-        ()
-    )
-    if is_universal:
-        for user in all_users:
-            user_id = user[0]
-            game_ids = await db.get_user_uids(user_id)
-            tip_msg = f"[{code}]"
-            for game_id in game_ids:
-                res = await redeem_code(game_id, code)
-                result[game_id] = res.msg
-                tip_msg += f"\n{game_id}: {res.msg}"
-                if res.code == 0 or res.code == 2152:
-                    success_count += 1
-                else:
-                    failure_count += 1
-            tip_msg += "\n发送 /关闭提醒 将不再接收该消息"
-            friend_list = await bot.get_friend_list()
-            friend_user_ids = [f['user_id'] for f in friend_list]
-            is_friend = user_id in friend_user_ids
-            if game_ids and is_friend and user[1]:
-                try:
-                    await bot.send_private_msg(
-                        user_id=user_id,
-                        message=tip_msg
-                    )
-                except Exception as e:
-                    logger.warning(e)
-    else:
-        for user in all_users:
-            user_id = user[0]
-            game_ids = await db.get_user_uids(user_id)
-            # 删掉区服不匹配的uid
-            new_game_ids: list[int] = []
-            for game_id in game_ids:
-                if get_server_id(game_id) in available_servers:
-                    new_game_ids.append(game_id)
-            if not new_game_ids:
-                continue
-            tip_msg = f"[{code}]"
-            for game_id in new_game_ids:
-                res = await redeem_code(game_id, code)
-                result[game_id] = res.msg
-                tip_msg += f"\n{game_id}: {res.msg}"
-                if res.code == 0 or res.code == 2152:
-                    success_count += 1
-                else:
-                    failure_count += 1
-            tip_msg += "\n发送 /关闭提醒 将不再接收该消息"
-            friend_list = await bot.get_friend_list()
-            friend_user_ids = [f['user_id'] for f in friend_list]
-            is_friend = user_id in friend_user_ids
-            if game_ids and is_friend and user[1]:
-                try:
-                    await bot.send_private_msg(
-                        user_id=user_id,
-                        message=tip_msg
-                    )
-                except Exception as e:
-                    logger.warning(e)
+#     result: dict[int, str] = {}
+#     success_count = 0
+#     failure_count = 0
+#     all_users = await db.query_all(
+#         "SELECT user_id, need_remind FROM users",
+#         ()
+#     )
+#     if is_universal:
+#         for user in all_users:
+#             user_id = user[0]
+#             game_ids = await db.get_user_uids(user_id)
+#             tip_msg = f"[{code}]"
+#             for game_id in game_ids:
+#                 res = await redeem_code(game_id, code)
+#                 result[game_id] = res.msg
+#                 tip_msg += f"\n{game_id}: {res.msg}"
+#                 if res.code == 0 or res.code == 2152:
+#                     success_count += 1
+#                 else:
+#                     failure_count += 1
+#             tip_msg += "\n发送 /关闭提醒 将不再接收该消息"
+#             friend_list = await bot.get_friend_list()
+#             friend_user_ids = [f['user_id'] for f in friend_list]
+#             is_friend = user_id in friend_user_ids
+#             if game_ids and is_friend and user[1]:
+#                 try:
+#                     await bot.send_private_msg(
+#                         user_id=user_id,
+#                         message=tip_msg
+#                     )
+#                 except Exception as e:
+#                     logger.warning(e)
+#     else:
+#         for user in all_users:
+#             user_id = user[0]
+#             game_ids = await db.get_user_uids(user_id)
+#             # 删掉区服不匹配的uid
+#             new_game_ids: list[int] = []
+#             for game_id in game_ids:
+#                 if get_server_id(game_id) in available_servers:
+#                     new_game_ids.append(game_id)
+#             if not new_game_ids:
+#                 continue
+#             tip_msg = f"[{code}]"
+#             for game_id in new_game_ids:
+#                 res = await redeem_code(game_id, code)
+#                 result[game_id] = res.msg
+#                 tip_msg += f"\n{game_id}: {res.msg}"
+#                 if res.code == 0 or res.code == 2152:
+#                     success_count += 1
+#                 else:
+#                     failure_count += 1
+#             tip_msg += "\n发送 /关闭提醒 将不再接收该消息"
+#             friend_list = await bot.get_friend_list()
+#             friend_user_ids = [f['user_id'] for f in friend_list]
+#             is_friend = user_id in friend_user_ids
+#             if game_ids and is_friend and user[1]:
+#                 try:
+#                     await bot.send_private_msg(
+#                         user_id=user_id,
+#                         message=tip_msg
+#                     )
+#                 except Exception as e:
+#                     logger.warning(e)
 
-    superusers = list(get_driver().config.superusers)
-    superuser = superusers[0] if superusers else ""
-    selfnames = list(get_driver().config.nickname)
-    selfname = selfnames[0] if selfnames else "桃子"
+#     superusers = list(get_driver().config.superusers)
+#     superuser = superusers[0] if superusers else ""
+#     selfnames = list(get_driver().config.nickname)
+#     selfname = selfnames[0] if selfnames else "桃子"
 
-    text = (
-        f'total: {success_count + failure_count}\n'
-        f'success: {success_count}\n'
-        f'failure: {failure_count}\n'
-    )
-    text += '```'
-    for game_id, tip in result.items():
-        text += f'\n{game_id}: {tip}'
-    text += '\n```'
+#     text = (
+#         f'total: {success_count + failure_count}\n'
+#         f'success: {success_count}\n'
+#         f'failure: {failure_count}\n'
+#     )
+#     text += '```'
+#     for game_id, tip in result.items():
+#         text += f'\n{game_id}: {tip}'
+#     text += '\n```'
 
-    msg = Message(MessageSegment.node_custom(
-        user_id=int(bot.self_id),
-        nickname=selfname,
-        content=Message(MessageSegment.node_custom(
-            user_id=int(bot.self_id),
-            nickname=selfname,
-            content=Message(MessageSegment(
-                type="markdown",
-                data={
-                    "content": text
-                }
-            ))
-        ))
-    ))
+#     msg = Message(MessageSegment.node_custom(
+#         user_id=int(bot.self_id),
+#         nickname=selfname,
+#         content=Message(MessageSegment.node_custom(
+#             user_id=int(bot.self_id),
+#             nickname=selfname,
+#             content=Message(MessageSegment(
+#                 type="markdown",
+#                 data={
+#                     "content": text
+#                 }
+#             ))
+#         ))
+#     ))
 
-    try:
-        await dhm_update.send(msg)
-    except Exception as e:
-        logger.error(e)
+#     try:
+#         await dhm_update.send(msg)
+#     except Exception as e:
+#         logger.error(e)
 
-    try:
-        await bot.send_private_msg(
-            user_id=int(superuser),
-            message=msg
-        )
-    except Exception as e:
-        logger.error(e)
+#     try:
+#         await bot.send_private_msg(
+#             user_id=int(superuser),
+#             message=msg
+#         )
+#     except Exception as e:
+#         logger.error(e)
--- src/plugins/ninja3/uid_bind.py
+++ src/plugins/ninja3/uid_bind.py
@@ -11,8 +11,8 @@ from nonebot.adapters.onebot.v11.helpers import (
     convert_chinese_to_bool
 )
 
-from .db_manager import DatabaseManager as db
-from .utils import query_uid, redeem_code, get_server_id
+# from .db_manager import DatabaseManager as db
+# from .utils import query_uid, redeem_code, get_server_id
 
 
 
@@ -26,174 +26,175 @@ uid_add = on_command(
 
 @uid_add.handle()
 async def _(event: MessageEvent, state: T_State, arg: Message = CommandArg()):
-    user_id = event.user_id
-    user_uids = await db.get_user_uids(user_id)
-    if len(user_uids) >= 5:
-        await uid_add.finish("您绑定的uid数量已达上限")
+    await uid_add.send("由于官方兑换接口已下线，此功能将不再维护")
+#     user_id = event.user_id
+#     user_uids = await db.get_user_uids(user_id)
+#     if len(user_uids) >= 5:
+#         await uid_add.finish("您绑定的uid数量已达上限")
     
-    state['user_id'] = user_id
-    if nums := extract_numbers(arg):
-        state['game_id'] = int(nums[0])
+#     state['user_id'] = user_id
+#     if nums := extract_numbers(arg):
+#         state['game_id'] = int(nums[0])
 
 
-@uid_add.got('game_id', prompt="请发送要绑定的uid")
-async def _(state: T_State, game_id = ArgStr('game_id')):
-    if len(game_id) != 9 and len(game_id) != 12:
-        await uid_add.finish("欸？应该是9或12位才对吧？")
-    game_id = int(game_id)
-    uid_info = await query_uid(game_id)
-    if not uid_info:
-        await uid_add.finish("这个uid好像不太对哦")
+# @uid_add.got('game_id', prompt="请发送要绑定的uid")
+# async def _(state: T_State, game_id = ArgStr('game_id')):
+#     if len(game_id) != 9 and len(game_id) != 12:
+#         await uid_add.finish("欸？应该是9或12位才对吧？")
+#     game_id = int(game_id)
+#     uid_info = await query_uid(game_id)
+#     if not uid_info:
+#         await uid_add.finish("这个uid好像不太对哦")
 
-    game_id_info = await db.query_one(
-        "SELECT game_id, user_id FROM gameids WHERE game_id = ?",
-        (game_id,)
-    )
-    if game_id_info:
-        await uid_add.finish(f"{game_id}已经被{game_id_info[1]}绑定过啦~")
+#     game_id_info = await db.query_one(
+#         "SELECT game_id, user_id FROM gameids WHERE game_id = ?",
+#         (game_id,)
+#     )
+#     if game_id_info:
+#         await uid_add.finish(f"{game_id}已经被{game_id_info[1]}绑定过啦~")
 
-    state['game_id'] = game_id
-    await uid_add.send("玩家信息:\n" + uid_info + "\n是否确认？")
-
-
-@uid_add.receive()
-async def _(event: MessageEvent, state: T_State):
-    msg = event.get_message()
-    if convert_chinese_to_bool(msg):
-        user_id: int = state['user_id']
-        game_id: int = state['game_id']
-
-        user_info = await db.query_one(
-            "SELECT user_id FROM users WHERE user_id = ?",
-            (user_id,)
-        )
-        if not user_info:
-            await db.execute(
-                "INSERT INTO users (user_id, need_remind) VALUES (?, ?)",
-                (user_id, True)
-            )
-        await db.execute(
-            "INSERT INTO gameids (game_id, user_id) VALUES (?, ?)",
-            (game_id, user_id)
-        )
-
-        user_uids = await db.get_user_uids(user_id)
-        uid_str = ""
-        for u in user_uids:
-            uid_str += f"\n{u}"
-        await uid_add.send(f"添加成功，当前已绑定uid:{uid_str}\n正在检查可用的兑换码··")
-
-        server_id = get_server_id(game_id)
-        codes = await db.query_all('''
-            SELECT DISTINCT gc.code 
-            FROM giftcodes gc
-            JOIN giftcode_servers gcs ON gc.code = gcs.code
-            WHERE gc.available = ? AND gcs.server_id = ?;
-        ''', (True, server_id))
-        if codes:
-            reply_msg = ''
-            for c in codes:
-                res = await redeem_code(game_id, c[0])
-                if res.code == 2151:    # 过期了
-                    await db.execute(
-                        "UPDATE giftcodes SET available = ? WHERE code = ?",
-                        (False, c[0])
-                    )
-                else:
-                    reply_msg += f'\n{c[0]}: {res.msg}'
-            await uid_add.send(reply_msg.strip(), at_sender=True)
-
-    else:
-        await uid_add.send("已取消")
+#     state['game_id'] = game_id
+#     await uid_add.send("玩家信息:\n" + uid_info + "\n是否确认？")
 
 
+# @uid_add.receive()
+# async def _(event: MessageEvent, state: T_State):
+#     msg = event.get_message()
+#     if convert_chinese_to_bool(msg):
+#         user_id: int = state['user_id']
+#         game_id: int = state['game_id']
+
+#         user_info = await db.query_one(
+#             "SELECT user_id FROM users WHERE user_id = ?",
+#             (user_id,)
+#         )
+#         if not user_info:
+#             await db.execute(
+#                 "INSERT INTO users (user_id, need_remind) VALUES (?, ?)",
+#                 (user_id, True)
+#             )
+#         await db.execute(
+#             "INSERT INTO gameids (game_id, user_id) VALUES (?, ?)",
+#             (game_id, user_id)
+#         )
+
+#         user_uids = await db.get_user_uids(user_id)
+#         uid_str = ""
+#         for u in user_uids:
+#             uid_str += f"\n{u}"
+#         await uid_add.send(f"添加成功，当前已绑定uid:{uid_str}\n正在检查可用的兑换码··")
+
+#         server_id = get_server_id(game_id)
+#         codes = await db.query_all('''
+#             SELECT DISTINCT gc.code 
+#             FROM giftcodes gc
+#             JOIN giftcode_servers gcs ON gc.code = gcs.code
+#             WHERE gc.available = ? AND gcs.server_id = ?;
+#         ''', (True, server_id))
+#         if codes:
+#             reply_msg = ''
+#             for c in codes:
+#                 res = await redeem_code(game_id, c[0])
+#                 if res.code == 2151:    # 过期了
+#                     await db.execute(
+#                         "UPDATE giftcodes SET available = ? WHERE code = ?",
+#                         (False, c[0])
+#                     )
+#                 else:
+#                     reply_msg += f'\n{c[0]}: {res.msg}'
+#             await uid_add.send(reply_msg.strip(), at_sender=True)
+
+#     else:
+#         await uid_add.send("已取消")
 
 
 
-uid_del = on_command("忍3解除绑定", aliases={"忍三解除绑定"}, priority=10, block=True)
 
 
-@uid_del.handle()
-async def _(event: MessageEvent, arg: Message = CommandArg()):
-    user_id = event.user_id
-    user_uids = await db.get_user_uids(user_id)
-    if not user_uids:
-        await uid_del.finish("你还没有绑定过uid呢")
+# uid_del = on_command("忍3解除绑定", aliases={"忍三解除绑定"}, priority=10, block=True)
+
+
+# @uid_del.handle()
+# async def _(event: MessageEvent, arg: Message = CommandArg()):
+#     user_id = event.user_id
+#     user_uids = await db.get_user_uids(user_id)
+#     if not user_uids:
+#         await uid_del.finish("你还没有绑定过uid呢")
     
-    if nums := extract_numbers(arg):
-        for n in nums:
-            msg = await _handle_del_uid(user_id, int(n))
-        await uid_del.finish(msg)
-    else:
-        uid_str = ""
-        for u in user_uids:
-            uid_str += f"\n{u}"
-        await uid_del.send(f"请发送要删除的uid: {uid_str}")
+#     if nums := extract_numbers(arg):
+#         for n in nums:
+#             msg = await _handle_del_uid(user_id, int(n))
+#         await uid_del.finish(msg)
+#     else:
+#         uid_str = ""
+#         for u in user_uids:
+#             uid_str += f"\n{u}"
+#         await uid_del.send(f"请发送要删除的uid: {uid_str}")
 
 
-@uid_del.receive()
-async def _(event: MessageEvent):
-    if nums := extract_numbers(event.get_message()):
-        user_id = event.user_id
-        msg = await _handle_del_uid(user_id, int(nums[0]))
-        await uid_del.finish(msg)
-    await uid_del.finish("已退出当前操作")
-
-
-
-async def _handle_del_uid(user_id: int, game_id: int) -> str:
-    user_uids = await db.get_user_uids(user_id)
-    if user_uids and game_id in user_uids:
-        await db.execute(
-            "DELETE FROM gameids WHERE game_id=? AND user_id=?",
-            (game_id, user_id)
-        )
-
-        uids = await db.get_user_uids(user_id)
-        uid_str = ""
-        for u in uids:
-            uid_str += f"\n{u}"
-        return f"删除成功，当前已绑定uid:{uid_str}"
-
-    return "你没有绑定过这个uid哦"
+# @uid_del.receive()
+# async def _(event: MessageEvent):
+#     if nums := extract_numbers(event.get_message()):
+#         user_id = event.user_id
+#         msg = await _handle_del_uid(user_id, int(nums[0]))
+#         await uid_del.finish(msg)
+#     await uid_del.finish("已退出当前操作")
 
 
 
+# async def _handle_del_uid(user_id: int, game_id: int) -> str:
+#     user_uids = await db.get_user_uids(user_id)
+#     if user_uids and game_id in user_uids:
+#         await db.execute(
+#             "DELETE FROM gameids WHERE game_id=? AND user_id=?",
+#             (game_id, user_id)
+#         )
+
+#         uids = await db.get_user_uids(user_id)
+#         uid_str = ""
+#         for u in uids:
+#             uid_str += f"\n{u}"
+#         return f"删除成功，当前已绑定uid:{uid_str}"
+
+#     return "你没有绑定过这个uid哦"
 
 
 
-remind_close = on_command('关闭提醒', priority=10, block=True)
-remind_open = on_command('开启提醒', priority=10, block=True)
-
-# 添加和取消提醒
-@remind_close.handle()
-async def _(event: PrivateMessageEvent):
-    if await db.get_user_uids(event.user_id):
-        await db.execute(
-            "UPDATE users SET need_remind=0 WHERE user_id=?",
-            (event.user_id,)
-        )
-        await remind_close.send("已关闭提醒\n如需开启请发送 /开启提醒")
-
-@remind_open.handle()
-async def _(event: PrivateMessageEvent):
-    if await db.get_user_uids(event.user_id):
-        await db.execute(
-            "UPDATE users SET need_remind=1 WHERE user_id=?",
-            (event.user_id,)
-        )
-        await remind_open.send('已开启提醒')
 
 
 
-uid_search = on_command("uid查询", priority=10, block=True)
+# remind_close = on_command('关闭提醒', priority=10, block=True)
+# remind_open = on_command('开启提醒', priority=10, block=True)
+
+# # 添加和取消提醒
+# @remind_close.handle()
+# async def _(event: PrivateMessageEvent):
+#     if await db.get_user_uids(event.user_id):
+#         await db.execute(
+#             "UPDATE users SET need_remind=0 WHERE user_id=?",
+#             (event.user_id,)
+#         )
+#         await remind_close.send("已关闭提醒\n如需开启请发送 /开启提醒")
+
+# @remind_open.handle()
+# async def _(event: PrivateMessageEvent):
+#     if await db.get_user_uids(event.user_id):
+#         await db.execute(
+#             "UPDATE users SET need_remind=1 WHERE user_id=?",
+#             (event.user_id,)
+#         )
+#         await remind_open.send('已开启提醒')
 
 
-@uid_search.handle()
-async def handle_search_uid(arg: Message = CommandArg()):
-    nums = extract_numbers(arg)
-    if not nums:
-        return
-    uid = int(nums[0])
-    user_info = await query_uid(uid)
-    await uid_search.finish(str(user_info))
+
+# uid_search = on_command("uid查询", priority=10, block=True)
+
+
+# @uid_search.handle()
+# async def handle_search_uid(arg: Message = CommandArg()):
+#     nums = extract_numbers(arg)
+#     if not nums:
+#         return
+#     uid = int(nums[0])
+#     user_info = await query_uid(uid)
+#     await uid_search.finish(str(user_info))
