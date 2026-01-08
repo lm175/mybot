632e039d010ca522bd784bc5a5ca2fb157af405e
lm175 <1756000830@qq.com>
2026-01-08 22:52:33 +0800

    update

--- src/plugins/resident/__init__.py
+++ src/plugins/resident/__init__.py
@@ -1,5 +1,10 @@
 from nonebot import on_request, on_notice
 from nonebot.adapters.onebot.v11 import Bot, NoticeEvent, FriendRequestEvent
+from nonebot import require, get_bot
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+
+import random, asyncio
 
 from .group_invite import *
 
@@ -24,3 +29,24 @@ async def _(bot: Bot, event: FriendRequestEvent):
 
 
 # 自动点赞
+###
+
+
+
+# key签到
+@scheduler.scheduled_job(
+    trigger="cron",
+    hour=0,          # 小时：0 点（24小时制）
+    minute=1,        # 分钟：0 分（可选，默认 0，显式指定更清晰）
+    id="daily_midnight_job",  # 任务唯一ID（必填，用于后续管理任务）
+)
+async def daily_midnight_task():
+    try:
+        bot: Bot = get_bot() # type: ignore
+        await asyncio.sleep(random.randint(0, 300))  # 随机延迟0-5分钟
+        await bot.send_private_msg(
+            user_id=3889050520,
+            message=Message("key签到")
+        )
+    except Exception as e:
+        print(f"发送消息失败：{e}")
